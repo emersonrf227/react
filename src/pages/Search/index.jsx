b770a19cf98744728df8943e16f2d0ac5ea26de0
Emerson Rodrigues de Freitas <emersonrf227@yahoo.com.br>
2019-05-13 23:19:16 -0300

    atualização
    Projeto

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,11 +1,82 @@
-import React, {Component} from 'react'; 
+import React, { Component } from 'react';
+
+import axios from 'axios';
+
+import {Link} from 'react-router-dom';
+
+import './style.css';
 
 class Search extends Component {
-render(){ 
-    return(
+    constructor() {
+        super();
+        this.onSearch = this.onSearch.bind(this);
 
-        <div>Search</div> 
+        this.state = {
 
+            results: [],
+        };
+    }
+
+    onSearch(event) {
+        console.log(event.currentTarget.value);
+
+        const value = event.currentTarget.value;
+
+
+
+
+
+        axios.get(`https://api.mercadolibre.com/sites/MLB/search?q=${value}`)
+            .then(({ data }) => {
+
+                this.setState({
+
+                    results: data.results,
+
+
+                });
+
+
+            })
+
+    }
+
+
+    renderResult(item) {
+        return(
+            <li key={item.id}>
+                <span> {item.id}</span>
+                <span> {item.title}</span>
+                <Link  to={`/produtos/${item.id}`} >
+                    Acessar 
+                </Link>
+            </li>
+           
+
+        )
+
+
+
+    }
+
+
+    render() {
+        return (
+
+            <div>
+                <div id="container"> 
+                    <div className="backAzul">
+                        
+                        <input type="text" className="inputSearch" onChange={this.onSearch} />
+                    </div>
+                    
+                </div>
+                <ul>
+                    {this.state.results.map(this.renderResult)}
+                </ul>
+            </div>
+            
+           
 
         );
     }
